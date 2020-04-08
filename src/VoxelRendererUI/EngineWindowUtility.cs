a39fcc9045c18b8f861d4f99350098b2f0fed885
Luke Parker <lukemparker@outlook.com>
2020-04-08 11:58:55 +1000

    Create EngineWindowUtility.cs
    Builds a pixel console engine from the settings

---
+++ src/VoxelRendererUI/EngineWindowUtility.cs
@@ -0,0 +1,18 @@
+﻿using csPixelGameEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoxelRendererUI
+{
+    internal static class EngineWindowUtility
+    {
+        public static PixelGameEngine CreateEngine(EngineWindowSettings settings)
+        {
+            var window = new GLWindow(settings.ScreenWidth, settings.ScreenHeight, settings.PixelWidth, settings.PixelHeight, settings.AppName);
+            var pixelGameEngine = new PixelGameEngine(settings.AppName);
+            pixelGameEngine.Construct(settings.ScreenWidth, settings.ScreenHeight, window);
+            return pixelGameEngine;
+        }
+    }
+}
