5f696b406688b217452ab29d042dbc1728621c9e
Luke Parker <lukemparker@outlook.com>
2020-04-08 11:58:17 +1000

    Create EngineWindowSettings.cs
    Wraps some used settings

---
+++ src/VoxelRendererUI/EngineWindowSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace VoxelRendererUI
+{
+    internal class EngineWindowSettings
+    {
+        public string AppName { get; set; }
+        public uint ScreenWidth { get; set; }
+        public uint ScreenHeight { get; set; }
+        public uint PixelWidth { get; set; }
+        public uint PixelHeight { get; set; }
+    }
+}
